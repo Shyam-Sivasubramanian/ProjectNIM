89a408d49ecc41c37f311a48ec53cbbae9f0175f
Shyam Sivasubramanian <96302076+Shyam-Sivasubramanian@users.noreply.github.com>
2022-04-12 09:26:06 -0400

    Create GameRunner.java

---
+++ GameRunner.java
@@ -0,0 +1,19 @@
+package projectNIM;
+
+
+import java.util.Scanner;
+
+public class  GameRunner {
+
+    public static void main(String args[]){
+        System.out.println("Welcome to the game of Nim");
+        System.out.println("Nim is played starting with piles or rows of objects. Players take turns taking any number of objects from one of the piles. Whoever takes the last of the objects wins! However, one is only allowed to take at most one half of the pile on a single turn! GLHF!");
+
+
+
+
+
+    }
+    
+
+}
