293d4efc62405d92959039fd404fd1470c394c02
Shyam Sivasubramanian <96302076+Shyam-Sivasubramanian@users.noreply.github.com>
2022-04-14 23:09:04 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,24 +1,15 @@
 package projectNIM;
+import java.util.Random;
 
 public class Board{
-     
+int yo;
+    public Board(){
+        yo = 0;
+    }
 
-
-public int populate(){
-
-    return 0;
+    public int populate(){
+        Random r = new Random();
+        int numPile = r.nextInt(50);
+        return numPile;
+    }
 }
-
-
-
-
-}
-
-
-
-
-
-
-
-
-// Shyam is Thicc Daddy
