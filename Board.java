9a034de135c4b23fa8aaeba096cac926c877c7c8
Shyam Sivasubramanian <96302076+Shyam-Sivasubramanian@users.noreply.github.com>
2022-04-14 23:09:36 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,15 +1,18 @@
 package projectNIM;
 import java.util.Random;
 
-public class Board{
-int yo;
-    public Board(){
-        yo = 0;
+public class Game {
+int x;
+    public Game(){
+        x=0;
+    }
+   
+
+    public int goFirst(){
+        Random r = new Random();
+        int y = r.nextInt(2);
+        return y;
     }
 
-    public int populate(){
-        Random r = new Random();
-        int numPile = r.nextInt(50);
-        return numPile;
-    }
+    
 }
